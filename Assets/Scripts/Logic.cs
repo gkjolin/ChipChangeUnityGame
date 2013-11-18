9a5563e02d9e176db2e522b7dadea4dc68b58a3f
Travis Irby <travis.irby@gmail.com>
2013-11-18 15:26:50 -0500

    Some Early Logic and Workin a Tiling System

---
+++ Assets/Scripts/Logic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logic : MonoBehaviour {
+
+	public GameObject testSprite;
+	public Vector2 moveSpeed = new Vector2(50f,0f);
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+		rigidbody2D.AddForce(moveSpeed);
+	}
+
+	void OnMouseUp()
+	{
+		RaycastHit2D ray = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		if (ray.collider != null && ray.collider.transform == this.gameObject.transform)
+			Instantiate(testSprite, transform.position, transform.rotation);
+	}
+}
---
+++ Assets/Scripts/SpriteTiling.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteTiling : MonoBehaviour {
+	
+	public Vector2 tileSize = new Vector2(1, 1);
+	public bool flipHorizontal = false;
+	public bool flipVertical = false;
+}
