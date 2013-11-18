a64e6a722de3483dfa2556cea0a1513ef5b832c2
Travis Irby <travis.irby@gmail.com>
2013-11-18 16:20:01 -0500

    Organized Assets, Added DragShape script

---
+++ Assets/Scripts/DragShape.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class DragShape : MonoBehaviour
+{
+	private Transform thisTransform;
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	private Vector3 originalPos;
+
+	void Start()
+	{
+		thisTransform = transform;
+		originalPos = thisTransform.position;
+	}
+
+	void OnMouseDown()
+	{
+		screenPoint = Camera.main.WorldToScreenPoint(thisTransform.position);
+		offset = thisTransform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		
+	}
+	
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint), offset;
+		thisTransform.position = curPosition;
+		
+	}
+	
+}
