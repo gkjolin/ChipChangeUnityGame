9b8d26a741a0a66d38fd4204e8fa10b04875c5a1
Travis Irby <travis.irby@gmail.com>
2013-11-18 11:27:39 -0500

    Testing new 2d tools and syntax

--- Assets/Logic.cs
+++ Assets/Logic.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class Logic : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject testSprite;
+
 	void Start () {
 	
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+	}
+
+	void OnMouseUp()
+	{
+		RaycastHit2D ray = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		if (ray.collider != null && ray.collider.transform == this.gameObject.transform)
+			Instantiate(testSprite, transform.position, transform.rotation);
 	}
 }
