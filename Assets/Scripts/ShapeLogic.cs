b7e63b3ae190f33e411a4d54b7594567d3487934
Travis Irby <travis.irby@gmail.com>
2013-11-18 20:42:53 -0500

    Worked on first shape - block

---
+++ Assets/Scripts/ShapeLogic.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeLogic : MonoBehaviour {
+	
+	public Vector2 moveSpeed = new Vector2(50f,0f);
+
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+		rigidbody2D.AddForce(moveSpeed);
+	}
+	
+}
---
+++ Assets/Scripts/SpawnerLogic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerLogic : MonoBehaviour {
+
+	public GameObject testSprite;
+
+	void OnMouseUp()
+	{
+		RaycastHit2D ray = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+		if (ray.collider != null && ray.collider.transform == this.gameObject.transform)
+		{
+			Instantiate(testSprite, transform.position, transform.rotation);
+			this.gameObject.SetActive(false);
+		}
+	}
+}
