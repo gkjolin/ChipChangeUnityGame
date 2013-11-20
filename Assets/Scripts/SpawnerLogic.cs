f82ae161b544c6d4fc0750a2dfc6dc37e5398ab0
Travis Irby <travis.irby@gmail.com>
2013-11-20 11:11:11 -0500

    Added LeanTween from github, tested workin in flash build

--- Assets/Scripts/SpawnerLogic.cs
+++ Assets/Scripts/SpawnerLogic.cs
@@ -10,7 +10,7 @@ public class SpawnerLogic : MonoBehaviour {
 //		RaycastHit2D ray = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 //		if (ray.collider != null && ray.collider.transform == this.gameObject.transform)
 //		{
-			Instantiate(testSprite, transform.position, transform.rotation);
+			Instantiate(testSprite, new Vector3 (transform.position.x,transform.position.y,0f), transform.rotation);
 			this.gameObject.SetActive(false);
 //		}
 	}
---
+++ Assets/testLeanTween.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testLeanTween : MonoBehaviour {
+	
+	Vector3 moveTweenVector3;
+	
+	// Use this for initialization
+	void Start () {
+		moveTweenVector3 = new Vector3(transform.position.x,transform.position.y+5,transform.position.z);
+		MoveTween();
+	}
+	
+	// Update is called once per frame
+	void MoveTween () {
+		LeanTween.move( gameObject, moveTweenVector3, 2.0f, new object[]{ "ease",LeanTweenType.easeOutElastic,"repeat",7});
+		//LeanTween.move(gameObject,moveTweenVector3,1f { "onComplete",MoveTween});
+	}
+}
